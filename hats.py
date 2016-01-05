c7fcc93a479e6ebb16cb26f09be64c493971f9b6
Michael Van Buren <mikevb@fastmail.fm>
2016-01-05 08:52:58 -0800

    tkinter folder select window if not automatically found

--- hats.py
+++ hats.py
@@ -1,28 +1,45 @@
-from winreg import OpenKey, EnumValue, CloseKey, HKEY_LOCAL_MACHINE as HKLM
+from winreg import OpenKey, EnumValue, CloseKey, HKEY_LOCAL_MACHINE as HKLM #to get steam install location
 from shutil import copy2 as copy
+from tkinter import Tk, filedialog
 from sys import exit
 import os
 import re
 
-def check_folder_exist(folder,foldername):
-    try:
-        os.stat(folder)
-    except FileNotFoundError:
-        print('Could not find {} folder.'.format(foldername))
-        input('Press ENTER to exit.')
-        exit(0)
+def exist_check(location,name):
+    folder = location
+    while True:
+        try:
+            os.stat(folder)
+            break
+        except FileNotFoundError:
+            if name == 'Steam':
+                return False
+            folder = folder_select(name)
+    return True
+
+def folder_select(name):
+    root = Tk()
+    root.withdraw()
+    folder = filedialog.askdirectory(parent=root,
+                                     initialdir='C:\\',
+                                     mustexist=True,
+                                     title='Select {} directory.'.format(name)
+                                     )
+    root.destroy()
+    return folder
 
 def main():
     steam_reg_key = OpenKey(HKLM,'SOFTWARE\\WOW6432Node\\Valve\\Steam')
     steam_dir = EnumValue(steam_reg_key,1)[1]
     CloseKey(steam_reg_key)
-    check_folder_exist(steam_dir,'Steam')
-
-    game_dir = '{}\\steamapps\\common\\Duck Game'.format(steam_dir)
-    check_folder_exist('{}\\DuckGame.exe'.format(game_dir),'Duck Game')
+    if !exist_check(steam_dir,'Steam'):
+        game_dir = folder_select('Duck Game')
+    else:
+        game_dir = '{}\\steamapps\\common\\Duck Game'.format(steam_dir)
+        exist_check('{}\\DuckGame.exe'.format(game_dir),'Duck Game')
 
     hat_dir = '{}\\hats'.format(os.getcwd())
-    check_folder_exist(hat_dir,'hat')
+    exist_check(hat_dir,'hat')
 
     for f in os.listdir(game_dir):
         if re.match('.*\.hat',f) or f == 'hatcredits.txt':
