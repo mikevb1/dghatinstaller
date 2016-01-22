016c9233703a58ec7e6ec6604150b4d867235460
Michael Van Buren <mikevb@fastmail.fm>
2016-01-22 14:34:59 -0800

    pep8 compliance

--- src/hats.py
+++ src/hats.py
@@ -1,11 +1,12 @@
-from winreg import OpenKey, EnumValue, CloseKey, HKEY_LOCAL_MACHINE as HKLM #to get steam install location
+from winreg import OpenKey, EnumValue, CloseKey, HKEY_LOCAL_MACHINE as HKLM
 from shutil import copy2 as copy
 from tkinter import Tk, filedialog
 from sys import exit
 import os
 import re
 
-def exist_check(location,name):
+
+def exist_check(location, name):
     folder = location
     while True:
         if os.path.isdir(folder):
@@ -13,6 +14,7 @@ def exist_check(location,name):
         else:
             folder = folder_select(name)
     return folder
+
 
 def folder_select(name):
     root = Tk()
@@ -27,16 +29,18 @@ def folder_select(name):
         exit(0)
     return folder
 
+
 def get_steam_dir():
     try:
-        steam_reg_key = OpenKey(HKLM,'SOFTWARE\\WOW6432Node\\Valve\\Steam')
+        steam_reg_key = OpenKey(HKLM, 'SOFTWARE\\WOW6432Node\\Valve\\Steam')
     except FileNotFoundError:
-        steam_reg_key = OpenKey(HKLM,'SOFTWARE\\Valve\\Steam')
+        steam_reg_key = OpenKey(HKLM, 'SOFTWARE\\Valve\\Steam')
     except:
         return
-    steam_dir = EnumValue(steam_reg_key,1)[1]
+    steam_dir = EnumValue(steam_reg_key, 1)[1]
     CloseKey(steam_reg_key)
     return steam_dir
+
 
 def main():
     steam_dir = get_steam_dir()
@@ -44,22 +48,22 @@ def main():
         game_dir = folder_select('Duck Game')
     else:
         game_dir = '{}\\steamapps\\common\\Duck Game'.format(steam_dir)
-        game_dir = exist_check(game_dir,'Duck Game')
+        game_dir = exist_check(game_dir, 'Duck Game')
 
     hat_dir = '{}\\hats'.format(os.getcwd())
-    hat_dir = exist_check(hat_dir,'hat')
+    hat_dir = exist_check(hat_dir, 'hat')
 
-    log = open('hats.log','w')
+    log = open('hats.log', 'w')
     log.write('Removing files:\n')
     for f in os.listdir(game_dir):
-        if re.match('.*\.hat',f) or f == 'hatcredits.txt':
-            os.remove('{}\\{}'.format(game_dir,f))
-            log.write('{}\\{}'.format(game_dir,f) + '\n')
+        if re.match('.*\.hat', f) or f == 'hatcredits.txt':
+            os.remove('{}\\{}'.format(game_dir, f))
+            log.write('{}\\{}'.format(game_dir, f) + '\n')
     log.write('\nCopying files:\n')
     for f in os.listdir(hat_dir):
-        srcfile = '{}\\{}'.format(hat_dir,f)
-        destfile = '{}\\{}'.format(game_dir,f)
-        copy(srcfile,destfile)
+        srcfile = '{}\\{}'.format(hat_dir, f)
+        destfile = '{}\\{}'.format(game_dir, f)
+        copy(srcfile, destfile)
         log.write(srcfile + ' -> ' + destfile + '\n')
     log.close()
 
