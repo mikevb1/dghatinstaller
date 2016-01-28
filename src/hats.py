94c9b554e036ab685b7d817a69ad502ba75098be
Michael Van Buren <mikevb@fastmail.fm>
2016-01-27 23:37:58 -0800

    improve log handling

--- src/hats.py
+++ src/hats.py
@@ -1,8 +1,9 @@
 """Removes old hats in Duck Game directory, moves new ones over."""
 from winreg import OpenKey, EnumValue, CloseKey, HKEY_LOCAL_MACHINE as HKLM
-from shutil import copy2 as copy
 from tkinter import Tk, filedialog, messagebox
+from shutil import copy2 as copy
 from sys import argv, exit
+import datetime as dt
 import os
 import re
 
@@ -78,6 +79,16 @@ def get_steam_dir():
     return steam_dir
 
 
+def log_from_main(main=argv[0]):
+    """Take name of script and change extension to 'log'."""
+    log = list(main)
+    while log[-1] != '.':
+        log.pop()
+    log.append('log')
+    log = ''.join(log)
+    return log
+
+
 def main():
     """Remove any hats in game directory and copies new ones over."""
     steam_dir = get_steam_dir()
@@ -90,19 +101,23 @@ def main():
     hat_dir = '{}\\hats'.format(os.getcwd())
     hat_dir = exist_check(hat_dir, 'hat')
 
-    log = open('hats.log', 'w')
-    log.write('Removing files:\n')
-    for f in os.listdir(game_dir):
-        if re.match('.*\.hat', f) or f == 'hatcredits.txt':
-            os.remove('{}\\{}'.format(game_dir, f))
-            log.write('{}\\{}'.format(game_dir, f) + '\n')
-    log.write('\nCopying files:\n')
-    for f in os.listdir(hat_dir):
-        srcfile = '{}\\{}'.format(hat_dir, f)
-        destfile = '{}\\{}'.format(game_dir, f)
-        copy(srcfile, destfile)
-        log.write(srcfile + ' -> ' + destfile + '\n')
-    log.close()
+    log_file = log_from_main()
+    log_now = dt.datetime.now().strftime('%d %b %Y - %H:%M:%S')
+
+    with open(log_file, 'a') as log:
+        log.write('Run Time: {}\n\n'.format(log_now))
+        log.write('Removing files:\n')
+        for f in os.listdir(game_dir):
+            if re.match('.*\.hat', f) or f == 'hatcredits.txt':
+                os.remove('{}\\{}'.format(game_dir, f))
+                log.write('{}\\{}'.format(game_dir, f) + '\n')
+        log.write('\nCopying files:\n')
+        for f in os.listdir(hat_dir):
+            srcfile = '{}\\{}'.format(hat_dir, f)
+            destfile = '{}\\{}'.format(game_dir, f)
+            copy(srcfile, destfile)
+            log.write('{} -> {}\n'.format(srcfile, destfile))
+        log.write('\n----------\n\n')
 
     done_box()
 
